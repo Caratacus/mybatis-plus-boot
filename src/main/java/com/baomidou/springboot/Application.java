ddb4ec65f97a9ffee3b673fb5ec77df34aa3b62d
Caratacus <330952486@qq.com>
2017-02-06 16:35:43 +0800

    集成kisso

--- src/main/java/com/baomidou/springboot/Application.java
+++ src/main/java/com/baomidou/springboot/Application.java
@@ -3,21 +3,28 @@ package com.baomidou.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @MapperScan("com.baomidou.springboot.mapper*")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	/**
 	 * <p>
-	 * 测试 RUN
-	 * <br>
+	 * 测试 RUN <br>
 	 * 1、http://localhost:8080/user/test1<br>
 	 * 2、http://localhost:8080/user/test2<br>
 	 * </p>
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("this project start with Spring Boot:");
+
 	}
 
 }
